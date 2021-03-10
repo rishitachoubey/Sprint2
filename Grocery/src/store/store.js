837cf9188dd93aaa2b740eda513c86568ecaff1d
Rupsi Kumari <66114602+Rupsi-Singh@users.noreply.github.com>
2021-03-10 17:11:57 +0530

    Add files via upload

---
+++ Grocery/src/store/store.js
@@ -0,0 +1,30 @@
+import { createStore, applyMiddleware,compose} from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from "./reducers";
+
+
+const initialState={};
+const middleWare=[thunk];
+
+
+const ReactReduxDevTools= window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+let store;
+
+if(window.navigator.userAgent.includes("Chrome")&&ReactReduxDevTools){
+    store=createStore(
+        rootReducer,
+        initialState,
+        compose(
+            applyMiddleware(...middleWare), 
+            ReactReduxDevTools)
+    );
+}else{
+    store=createStore(
+        rootReducer,
+        initialState,
+        compose(
+            applyMiddleware(...middleWare))
+    )
+            
+}
+export default store;
