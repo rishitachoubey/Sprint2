032a471e1239d219fcaee95b724616099599cc2b
JayasriVejendla <76989906+JayasriVejendla@users.noreply.github.com>
2021-03-10 16:11:54 +0530

    Update OrderReviewController.java

--- onlinegrocery/src/main/java/com/cg/onlinegrocery/web/OrderReviewController.java
+++ onlinegrocery/src/main/java/com/cg/onlinegrocery/web/OrderReviewController.java
@@ -1,24 +1,36 @@
 package com.cg.onlinegrocery.web;
 
 import java.util.Optional;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.cg.onlinegrocery.domain.Order_Review;
 import com.cg.onlinegrocery.repository.OrderReviewRepository;
 import com.cg.onlinegrocery.service.OrderReviewService;
 
+/**
+ * This is the controller class which provides the end points foe the
+ * application
+ * 
+ * @author Jayasri Vejendla
+ *
+ */
 
 @RestController
-
+@CrossOrigin
+@RequestMapping("/api")
 public class OrderReviewController {
 	
 	@Autowired
@@ -27,8 +39,8 @@ public class OrderReviewController {
 	private OrderReviewRepository repo;
 	
 	@PostMapping("/ConfirmOrder")
-	public ResponseEntity<String> ConfirmOrder( @RequestBody Order_Review order)
-	 {
+	public ResponseEntity<String> ConfirmOrder( @RequestBody Order_Review order) {
+	
 		order = service.ConfirmOrder(order);
 		return new ResponseEntity<String>("Order has been Confirm successfully" + order.getOrder_id(), HttpStatus.OK);
 
@@ -40,12 +52,18 @@ public class OrderReviewController {
 		return new ResponseEntity<String>("Order has been updated successfully" + order.getOrder_id(), HttpStatus.OK);
 	}
 
-	@DeleteMapping("/CancelOrder")
-	public ResponseEntity<String> CancelOrder(@RequestBody Order_Review order) {
+	@DeleteMapping("/DeleteOrder/{orderId}")
+	//public ResponseEntity<String> DeleteOrder(@RequestBody Order_Review order) {
 //		service.CancelOrderById(orderId);
-		repo.delete(order);
-		return new ResponseEntity<String>("Order has been cancelled successfully" + order.getOrder_id(), HttpStatus.OK);
-	}
+		//repo.delete(order);
+		//return new ResponseEntity<String>("Order has been cancelled successfully" + order.getOrder_id(), HttpStatus.OK)
+		//public Optional<Order_Review> DeleteOrderByID(@PathVariable int orderId) {
+			//return service.DeleteOrder(orderId);
+		//}
+		public ResponseEntity<String> deleteOrder(@PathVariable("orderId") int orderId) {
+	 		int result = service.removeOrder(orderId);
+	 		return new ResponseEntity<String>("order with id "+result+ " deleted succesfully:", HttpStatus.OK);
+	 	}
 	
 	@GetMapping("/viewallOrder")
 	public Iterable<Order_Review> viewallOrders(){
@@ -58,4 +76,6 @@ public class OrderReviewController {
 
 		
 	}
+
+
 
