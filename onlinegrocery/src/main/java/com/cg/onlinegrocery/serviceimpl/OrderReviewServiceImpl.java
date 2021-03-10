1892dcaa260cf513c49b6dad2a3fabe4584e97c1
JayasriVejendla <76989906+JayasriVejendla@users.noreply.github.com>
2021-03-10 16:10:41 +0530

    Update OrderReviewServiceImpl.java

--- onlinegrocery/src/main/java/com/cg/onlinegrocery/serviceimpl/OrderReviewServiceImpl.java
+++ onlinegrocery/src/main/java/com/cg/onlinegrocery/serviceimpl/OrderReviewServiceImpl.java
@@ -8,7 +8,13 @@ import com.cg.onlinegrocery.exception.OrderNotFoundException;
 import com.cg.onlinegrocery.repository.OrderReviewRepository;
 import com.cg.onlinegrocery.service.OrderReviewService;
 import com.cg.onlinegrocery.serviceimpl.OrderReviewServiceImpl;
-
+/**
+ * This service implementation class will provide implementations to the service
+ * layer
+ * 
+ * @author Jayasri Vejendla
+ *
+ */
 
 @Service
 
@@ -20,29 +26,25 @@ public class OrderReviewServiceImpl  implements OrderReviewService {
 
 	@Override
 	public Order_Review ConfirmOrder(Order_Review order) {
-		int Order_id = order.getOrder_id();
-		if (repository.findById(Order_id) != null)
+		//int Order_id = order.getOrder_id();
+		//if (repository.findById(Order_id) != null)
 			
 		return repository.save(order);
-		else
-			throw new OrderNotFoundException("Order for confirm is not present with the id" + Order_id);
+		//else
+			//throw new OrderNotFoundException("Order for confirm is not present with the id" + Order_id);
 	}
 	
 	@Override
 	public Order_Review UpdateOrder(Order_Review order) {
-		int Order_id = order.getOrder_id();
-		if (repository.findById(Order_id) != null)
+		//int Order_id = order.getOrder_id();
+		//if (repository.findById(Order_id) != null)
 			
 		return repository.save(order);
-		else
-			throw new OrderNotFoundException("Order for update is not present with the id" + Order_id);
+		//else
+			//throw new OrderNotFoundException("Order for update is not present with the id" + Order_id);
 	}
 	
-	@Override
-	public void CancelOrderById(int id)
-    {
-   	 repository.deleteById(id);
-    }
+	
 	
 	@Override
 	public Iterable<Order_Review> getAllOrders() {
@@ -57,7 +59,33 @@ public class OrderReviewServiceImpl  implements OrderReviewService {
 		
 		
 	}
+
+	//@Override
+	//public Optional<Order_Review> DeleteOrder(int OrderId) {
+		// TODO Auto-generated method stub
 		
-}
+	   //	return repository.findById(OrderId);
+		
+	//}  
+	public int removeOrder(int OrderId) throws OrderNotFoundException{
+		Optional<Order_Review> orderreview = repository.findById(OrderId);
+		if(orderreview==null)
+		{
+			throw new OrderNotFoundException("No order found with this id");
+		}
+		else
+		{
+		repository.delete(orderreview.get());
+	   	    return OrderId;	
+		}
+   	    
+	}
+	
+	}
+
+	
+
+	
+
 		
 
